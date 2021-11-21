6c73b365d2d9d9c302fdf8e7e469db7b218b366d
Tanmay-Agarwal-24 <80101441+Tanmay-Agarwal-24@users.noreply.github.com>
2021-11-21 17:25:05 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,10 @@
+class atm(object):
+    def __init__(self, cardNum, pinNum):
+        self.cardNum = cardNum
+        self.pinNum = pinNum
+        
+    def cashWithdraw(self):
+        print('Withdrawing 10')
+
+    def balanceEnquiry(self):
+        print('yourbalnce is 10')
